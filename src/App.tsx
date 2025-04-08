ea3d16af20049995c541b7007005c6c6b8edfbe6
Rafael Mota <rmorixa@rmrx.local>
2025-04-08 10:50:13 -0300

    [PB_0003] - Organization of React components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
+import { Container } from './components/container';
+import { Heading } from './components/heading';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Hello World!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium sed
-        itaque, quo numquam magnam illum officiis libero distinctio sit, et
-        consequuntur quisquam porro eaque. Veniam, deleniti voluptatem.
-        Similique, doloribus quisquam.
-      </p>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 }
