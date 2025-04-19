136d2c5c1ec029999bf6215e6948fb768e27b33e
Rafael Mota <rmorixa@rmrx.local>
2025-04-19 11:14:31 -0300

    [PB_0004] - Criation of the Logo component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import './styles/theme.css';
-import './styles/global.css';
 import { Container } from './components/container';
 import { Heading } from './components/heading';
+import { Logo } from './components/logo';
+
+import './styles/theme.css';
+import './styles/global.css';
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>LOGO</Heading>
+        <Logo />
       </Container>
+
       <Container>
         <Heading>MENU</Heading>
       </Container>
